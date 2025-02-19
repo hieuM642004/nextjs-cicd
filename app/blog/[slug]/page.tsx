200fd394bbaa564815142e9f01705fcb90a07133
Lê Minh Hiếu <hieu78544@gmail.com>
2025-02-19 08:17:15 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,5 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>Hieu</p></>;
+  return <><h1>Slug: {params.slug}</h1><p>Hieu</p><p>Hello CI CD</p></>;
 }
